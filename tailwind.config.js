99d488223f97fb4c1b11819aa6800dac4c8bc8e1
RandaAlfizah <randaalfiizh@gmail.com>
2024-03-23 16:19:05 +0700

    add corect image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      primary:["Caveat"],
+      body:["inter"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
 
